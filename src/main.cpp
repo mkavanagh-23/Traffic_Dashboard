7a2ec4ddb6452646ea8e880499a09a8fd75bdff6
Matthew Kavanagh <matthewfkavanagh@icloud.com>
2025-01-07 21:19:22 -0500

    Clean up CMake and helloworld for clean start

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,22 @@
 #include <thread>
 #include <iostream>
 
+// Parse and store information from NYSDOT REST API
+// Parse and store information for MCNY RSS feed
+// GUI Window with a top bar that allows for picking between markets
+// Left sidebar contains a selectable list of currently active incidents
+// Incidents are refreshed every one minute
+// Main content pane contains a "Details" view that contains expanded details included an embedded video stream (if available) and an embedded map
+// Do we want to display embedded video feed, or just keyframes?
+//
+// Incorporate JSON and XML/RSS parsing
+// Learn how to incorporate API key safely into project (perhaps sourced from ENV)?
+// Stream videos from an m3u8 playlist (obtained vis REST API)
+// Mapping coordinates to regions and markets
+// Severity coding
+// Embed Goole Maps objects
+// Create a dynamic graphical interface
+
 int main(int argc, char** argv)
 {
   // Check for valid arguments
