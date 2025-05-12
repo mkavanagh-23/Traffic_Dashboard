7a1029b5b8e572c69517a2f00a21c3b10b16376e
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-05-12 14:35:17 -0400

    Add subquery script

---
+++ database/scripts/subquery.sql
@@ -0,0 +1,31 @@
+USE mfkavanagh_255;
+
+-- subquery.sql
+-- List the most recently updated event in each region
+
+SELECT
+  traffic_events.event_id AS id,
+  event_categories.name AS type,
+  CONCAT_WS(' ', main_roadways.name, roadway_suffix.abbreviation) AS roadway,
+  traffic_events.travel_direction,
+  traffic_events.details,
+  market_regions.name AS market,
+  traffic_events.date_updated
+FROM traffic_events 
+JOIN event_categories
+  ON traffic_events.category_id = event_categories.category_id
+JOIN main_roadways
+  ON traffic_events.roadway_id = main_roadways.roadway_id
+LEFT JOIN roadway_suffix    -- Outer join to include roadways without suffixes
+  ON main_roadways.suffix_id = roadway_suffix.suffix_id
+JOIN market_regions
+  ON main_roadways.region_id = market_regions.region_id
+WHERE traffic_events.date_updated = (
+  SELECT 
+    MAX(te.date_updated)
+  FROM traffic_events te
+  JOIN main_roadways mr
+    ON mr.roadway_id = te.roadway_id
+  WHERE mr.region_id = main_roadways.region_id
+)
+ORDER BY market_regions.name ASC;
